25bf7d974465d0de41186074d5d164f21f963099
Nahom <nahomhaile_19@outlook.com>
2019-11-30 12:24:14 +0000

    Adding Currency Converter app file

---
+++ CurrencyConverter/app/src/main/java/learningprogramming/academy/currencyconverter/MainActivity.java
@@ -0,0 +1,37 @@
+package learningprogramming.academy.currencyconverter;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private EditText exchangeTotal;
+    private EditText amountEntered;
+    private Button submitExchange;
+    private Spinner spinner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent){
+
+    }
+}
